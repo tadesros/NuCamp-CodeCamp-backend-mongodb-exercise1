fe3264eb8233c2bf6579b938a5778d06dd23c956
tadesros <tomadesrosiers@gmail.com>
2021-11-12 22:12:22 -0500

    node callback hell and promises

--- operations.js
+++ operations.js
@@ -1,60 +1,19 @@
-//LIKE CRUD OPERATIONS
-
-
-
-//4 methods to update documents
-//ASSERT module require and strict vertion
-const assert = require('assert').strict;
-//Method 1:
-//parameters: db, document to insert
-//collection it is in
-//callback for it
-exports.insertDocument = (db, document, collection, callback) => {
-    //collection argument should be string "name"
+exports.insertDocument = (db, document, collection) => {
     const coll = db.collection(collection);
-    //call can now interact with a specific collection
-    //document and second callback
-    coll.insertOne(document, (err, result) => {
-        assert.strictEqual(err, null);
-        callback(result);
-    });
-};
-//Method: list all the documents
-//Just database name 
-exports.findDocuments = (db, collection, callback) => {
-
-    //Get access to the collection
-    const coll = db.collection(collection);
-
-    //Find empty so get all documents
-    //put the documents into an array
-    coll.find().toArray((err, docs) => {
-        assert.strictEqual(err, null);
-        callback(docs);
-    });
-
+    return coll.insertOne(document);
 };
 
-//Method: remove document
-exports.removeDocument = (db, document, collection, callback) => {
+exports.findDocuments = (db, collection) => {
     const coll = db.collection(collection);
-    //Delete one method
-    //document and a callback
-    //document is a javascript object
-    coll.deleteOne(document, (err, result) => {
-        assert.strictEqual(err, null);
-        callback(result);
-    });
+    return coll.find({}).toArray();
 };
 
-//Method: update document
-exports.updateDocument = (db, document, update, collection, callback) => {
+exports.removeDocument = (db, document, collection) => {
     const coll = db.collection(collection);
-    //Update $set -> update operator and 
-    //third is null
-    //forth error or result of the operation
-    coll.updateOne(document, { $set: update }, null, (err, result) => {
-        assert.strictEqual(err, null);
-        callback(result);
-    });
+    return coll.deleteOne(document);
+};
+
+exports.updateDocument = (db, document, update, collection) => {
+    const coll = db.collection(collection);
+    return coll.updateOne(document, { $set: update }, null);
 };
